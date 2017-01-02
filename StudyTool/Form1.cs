8c6591ad22f09b4c0b8e0713ce2aa61111a26506
Devon Honig <devon.honig@wsu.edu>
2017-01-01 22:22:23 -0800

    Base of Text Editor Created

--- StudyTool/Form1.cs
+++ StudyTool/Form1.cs
@@ -16,5 +16,75 @@ namespace StudyTool
 		{
 			InitializeComponent();
 		}
+
+		private void newToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Clear();
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void openToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+
+			ofd.Filter = "TextFiles (.txt)|*.txt";
+			ofd.Title = "Open a file...";
+
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				System.IO.StreamReader SR = new System.IO.StreamReader(ofd.FileName);
+				richTextBox1.Text = SR.ReadToEnd();
+				SR.Close();
+			}
+		}
+
+		private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog save = new SaveFileDialog();
+
+			save.Filter = "Text Files (.txt)|*.txt";
+			save.Title = "Save file...";
+
+			if (save.ShowDialog() == DialogResult.OK)
+			{
+				System.IO.StreamWriter write = new System.IO.StreamWriter(save.FileName);
+				write.Write(richTextBox1.Text);
+				write.Close();
+			}
+		}
+
+		private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Undo();
+		}
+
+		private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Redo();
+		}
+
+		private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Cut();
+		}
+
+		private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Copy();
+		}
+
+		private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Paste();
+		}
+
+		private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			richTextBox1.SelectAll();
+		}
 	}
 }
